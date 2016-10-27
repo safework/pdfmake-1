94701a81526078669f0fec497da9c5447f449fcb
Chloe Fitton <c.fitton@tlrg.com.au>
2016-10-27 12:31:17 +1100

    updated build

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,7 @@ module.exports = {
   entry: './src/browser-extensions/pdfMake.js',
   output: {
     path: path.join(__dirname, './build'),
-    filename: 'pdfmake.js',
-    name: 'pdfMake',
-    libraryTarget: 'umd',
-    umdNamedDefine: true
+    filename: 'pdfmake.js'
   },
   resolve: {
     alias: {
